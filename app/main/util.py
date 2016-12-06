147c56b4c3c62b58d669f8d1e0d64042a38aa90f
mayne <dreama9ain@gmail.com>
2016-12-06 15:25:13 +0800

    update

--- app/main/util.py
+++ app/main/util.py
@@ -2,7 +2,8 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 from bs4 import element
-
+from . views import  POST_PATH
+from collections import Counter
 header = {
     'user-agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
 }
@@ -33,6 +34,47 @@ class TPB():
             return res_list,True
         except:
             return res_list,False
+
+def time_format(unformat_time):
+    m_dict={
+        "Jan":"01",
+        "Feb":"02",
+        "Mar":"03",
+        "Apr":"04",
+        "May":"05",
+        "Jun":"06",
+        "Jul":"07",
+        "Aug":"08",
+        "Sep":"09",
+        "Oct":"10",
+        "Nov":"11",
+        "Dec":"12"
+    }
+    t = unformat_time.split("   ")[1]
+    time = t.split(" ")
+    m = m_dict[time[1]]
+    d = time[2]
+    y = time[4]
+    format_time = "{0}-{1}-{2}".format(y,m,d)
+    return  format_time
+
+def get_tags():
+    index = POST_PATH+"SUMMARY.md"
+    with open(index,'r',encoding='utf-8') as f:
+        text = f.read()
+        i = re.findall("\*\s{1}\[([\u4E00-\u9FA5\-\w \&\/\、\(\)]+)\]\(([\w\d_ \.\-]+)\)(#[\w,]+#)*",text)
+        all_tags = []
+        for *_asd,tags in i:
+            tags = tags[1:-1].split(",")
+            for tag in tags:
+                all_tags.append(tag)
+        ctn  =Counter()
+        for i in all_tags:
+            ctn[i] += 1
+        a_tags = dict(ctn)
+        if "" in all_tags:
+            del a_tags[""]
+    return a_tags
 
 if __name__=="__main__":
     res,flag = TPB("westworld","S01E02").get_magnet_info()
--- app/main/views.py
+++ app/main/views.py
@@ -1,54 +1,9 @@
 from .. import md
 from . import main
 from flask import render_template,request,flash
-from .util import TPB
+from .util import TPB,get_tags,time_format
 from ..config import POST_PATH
-from collections import Counter
 import os,re
-
-PER_PAGE=20
-
-
-def time_format(unformat_time):
-    m_dict={
-        "Jan":"01",
-        "Feb":"02",
-        "Mar":"03",
-        "Apr":"04",
-        "May":"05",
-        "Jun":"06",
-        "Jul":"07",
-        "Aug":"08",
-        "Sep":"09",
-        "Oct":"10",
-        "Nov":"11",
-        "Dec":"12"
-    }
-    t = unformat_time.split("   ")[1]
-    time = t.split(" ")
-    m = m_dict[time[1]]
-    d = time[2]
-    y = time[4]
-    format_time = "{0}-{1}-{2}".format(y,m,d)
-    return  format_time
-
-def get_tags():
-    index = POST_PATH+"SUMMARY.md"
-    with open(index,'r',encoding='utf-8') as f:
-        text = f.read()
-        i = re.findall("\*\s{1}\[([\u4E00-\u9FA5\-\w \&\/\、\(\)]+)\]\(([\w\d_ \.\-]+)\)(#[\w,]+#)*",text)
-        all_tags = []
-        for *_asd,tags in i:
-            tags = tags[1:-1].split(",")
-            for tag in tags:
-                all_tags.append(tag)
-        ctn  =Counter()
-        for i in all_tags:
-            ctn[i] += 1
-        a_tags = dict(ctn)
-        if "" in all_tags:
-            del a_tags[""]
-    return a_tags
 
 @main.route("/tags")
 def tags_cloud():
@@ -66,7 +21,7 @@ def tags(tag):
         for name,url,tags in i:
             tags = tags[1:-1].split(",")
             tags = [tag+str(hash(tag)%6+1) for tag in tags]
-            gitlog = os.popen("cd /root/blog ; git log {0}".format(url)).read()
+            gitlog = os.popen("cd {0} ; git log {1}".format(POST_PATH,url)).read()
             x = re.findall("Date:([\w\d :+]+)\+",gitlog)
             sub_time = time_format(x[-1])
             posts.append((name,url,tags,sub_time))
@@ -77,7 +32,7 @@ def post_v2(name):
     file  = POST_PATH+name+".md"
     with open(file,'r',encoding='utf-8') as f:
         content = md.convert(f.read())
-    gitlog = os.popen("cd /root/blog ; git log {0}.md".format(name)).read()
+    gitlog = os.popen("cd {0} ; git log {1}.md".format(POST_PATH,name)).read()
     x = re.findall("Date:([\w\d :+]+)\+",gitlog)
     sub_time = time_format(x[-1])
     up_time = time_format(x[0])
@@ -93,7 +48,7 @@ def post_by_summary():
         i = re.findall("\*\s{1}\[([\u4E00-\u9FA5\-\w \&\/\、\(\)]+)\]\(([\w\d_ \.\-]+)\)(#[\w,]+#)*",text)
         posts=[]
         for name,url,tags in i:
-            gitlog = os.popen("cd /root/blog ; git log {0}".format(url)).read()
+            gitlog = os.popen("cd {0}; git log {1}".format(POST_PATH,url)).read()
             x = re.findall("Date:([\w\d :+]+)\+",gitlog)
             sub_time = time_format(x[-1])
             tags = tags[1:-1].split(",")
@@ -105,28 +60,22 @@ def post_by_summary():
 
 @main.route("/book_hook",methods=['POST'])
 def bookhook():
-    if request.method=='POST':
-        os.system("cd /root/blog ; git pull")
+    '''
+    this hook is used to update blog posts
+    '''
+    if request.method=='POST' :
+        os.system("cd {0} ; git pull".format(POST_PATH))
     return "ok"
 
-# @main.route('/manage')
-# @login_required
-# def manage():
-#     all_posts = [(i.title,i.subtime,i.id) for i in Post.query.all()]
-#     return render_template("manage.html",posts=all_posts)
 
 @main.route('/hook',methods=['POST'])
 def hook():
-    #request.get_data()
+    '''
+    this hook is used to update blog program
+    '''
     os.system("cd /root/gine ; git pull ")
     os.popen(" pkill gunicorn && gunicorn manage:app")
     return "ok"
-
-@main.route("/od")
-def dulfy():
-    if request.method == "POST":
-        email = request.form['email']
-
 
 @main.route("/meiju",methods=['POST','GET'])
 def meiju():
@@ -139,92 +88,3 @@ def meiju():
         return render_template("meiju.html",res=res,flag=flag)
     else:
         return render_template("meiju_kong.html")
-
-
-# @main.route('/summary',methods=['POST','GET'])
-# def index():
-#     page = request.args.get('page',1,type=int)
-#     pagination = Post.query.order_by(Post.subtime.desc()).paginate(page,per_page=PER_PAGE,error_out=False)
-#     posts = pagination.items
-#     return render_template('allpost.html',posts=posts,pagination=pagination)
-
-# @main.route('/p/<int:id>')
-# def post(id):
-#     post = Post.query.get_or_404(id)
-#     content = post.content
-#     tag = post.tag.split(",")
-#     content = md.convert(content)
-#     return render_template('p.html',post=post,content = content,tag=tag)
-
-
-# @main.route('/subpost',methods=['POST','GET'])
-# @login_required
-# def sub_post():
-#     if request.method== 'POST':
-#         title= request.form["title"]
-#         tag = request.form["tag"]
-#         content = request.form["content"]
-#         #form.title.data=""
-#         #form.content.data=''
-#         #form.tag.data=""
-#         post = Post(title=title,tag=tag,content=content)
-#         db.session.add(post)
-#         db.session.commit()
-#         return  redirect(url_for(".post",id=post.id))
-#
-#     return render_template('sub_post_v2.html')
-#
-# @main.route('/edit/<int:id>',methods=['POST','GET'])
-# @login_required
-# def edit(id):
-#     post = Post.query.get_or_404(id)
-#     form = PostForm()
-#     if form.validate_on_submit():
-#         post.title= form.title.data
-#         post.content= form.content.data
-#         db.session.add(post)
-#         db.session.commit()
-#         flash("文章已经更新")
-#         return  redirect(url_for('.post',id=post.id))
-#     form.title.data = post.title
-#     form.content.data = post.content
-#     return render_template('edit_post.html',form=form,p_id= post.id)
-
-#
-# @main.route('/login',methods=['POST','GET'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user is not None and user.verify_password(form.password.data):
-#             login_user(user,form.remember_me.data)
-#             return redirect(url_for('main.index'))
-#         flash('账号或密码错误')
-#     return render_template('login.html',form=form)
-#
-# @main.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash("you have been logged out.")
-#     return redirect(url_for('main.index'))
-
-
-# @main.route('/wiki/<word>')
-# def wiki(word):
-#     from mwclient import  Site
-#     import  mwclient
-#     ua = 'MyCoolTool. Run by User:mayne. Using mwclient/' + mwclient.__ver__
-#     site = mwclient.Site(('http', 'gw2.huiji.wiki'),clients_useragent=ua,path='/')
-#     site.login('mayne','since2013')
-#     page = site.pages[word]
-#     page_text = page.text()[0:50]
-#     return  page_text
-#
-# @main.route('/search')
-# def search():
-#     pass
-#
-# @main.route('/test')
-# def webtest():
-#     return render_template('logint.html')
