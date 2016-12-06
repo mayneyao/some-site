e018bc34dc4ec67def25a6345b6f03d4041aefe9
mayne <dreama9ain@gmail.com>
2016-12-06 14:41:41 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,20 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_moment import Moment
-from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
-from flask_pagedown import PageDown
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_moment import Moment
+#from flask_bootstrap import Bootstrap
+#from flask_login import LoginManager
+#from flask_pagedown import PageDown
 import markdown
 from .config import sql_user,sql_pwd,sql_ip,sql_name,SECRET_KEY
 
 md = markdown.Markdown(extensions=['markdown.extensions.codehilite','markdown.extensions.extra'])
-bootstrap = Bootstrap()
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'login'
-moment = Moment()
-db = SQLAlchemy()
-pagedown = PageDown()
+#bootstrap = Bootstrap()
+#login_manager = LoginManager()
+#login_manager.session_protection = 'strong'
+#login_manager.login_view = 'login'
+#moment = Moment()
+#db = SQLAlchemy()
+#pagedown = PageDown()
 
 def create_app():
     app = Flask(__name__)
@@ -22,17 +22,17 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(sql_user,sql_pwd,sql_ip,sql_name)
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] =True
     app.debug=True
-    bootstrap.init_app(app)
-    moment.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-    pagedown.init_app(app)
+    #bootstrap.init_app(app)
+    #moment.init_app(app)
+    #db.init_app(app)
+    #login_manager.init_app(app)
+    #pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .api_1_0 import  api as api_1_0_blueprint
-    app.register_blueprint(api_1_0_blueprint,url_prefix="/api/v1.0")
+    #from .api_1_0 import  api as api_1_0_blueprint
+    #app.register_blueprint(api_1_0_blueprint,url_prefix="/api/v1.0")
 
     return app
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,12 @@
-from .. import md,db
+from .. import md
 from . import main
-from flask import render_template,session,flash,redirect,url_for,request
-from ..models import Post,User
-from .forms import PostForm,LoginForm
-from flask.ext.login import login_user,login_required,logout_user
+from flask import render_template,request
 from .util import TPB
 from ..config import POST_PATH
 from collections import Counter
-import json,os,re
+import os,re
 
 PER_PAGE=20
-
 
 
 def time_format(unformat_time):
@@ -113,11 +109,11 @@ def bookhook():
         os.system("cd /root/blog ; git pull")
     return "ok"
 
-@main.route('/manage')
-@login_required
-def manage():
-    all_posts = [(i.title,i.subtime,i.id) for i in Post.query.all()]
-    return render_template("manage.html",posts=all_posts)
+# @main.route('/manage')
+# @login_required
+# def manage():
+#     all_posts = [(i.title,i.subtime,i.id) for i in Post.query.all()]
+#     return render_template("manage.html",posts=all_posts)
 
 @main.route('/hook',methods=['POST'])
 def hook():
@@ -145,90 +141,90 @@ def meiju():
         return render_template("meiju_kong.html")
 
 
-@main.route('/summary',methods=['POST','GET'])
-def index():
-    page = request.args.get('page',1,type=int)
-    pagination = Post.query.order_by(Post.subtime.desc()).paginate(page,per_page=PER_PAGE,error_out=False)
-    posts = pagination.items
-    return render_template('allpost.html',posts=posts,pagination=pagination)
+# @main.route('/summary',methods=['POST','GET'])
+# def index():
+#     page = request.args.get('page',1,type=int)
+#     pagination = Post.query.order_by(Post.subtime.desc()).paginate(page,per_page=PER_PAGE,error_out=False)
+#     posts = pagination.items
+#     return render_template('allpost.html',posts=posts,pagination=pagination)
 
-@main.route('/p/<int:id>')
-def post(id):
-    post = Post.query.get_or_404(id)
-    content = post.content
-    tag = post.tag.split(",")
-    content = md.convert(content)
-    return render_template('p.html',post=post,content = content,tag=tag)
-
-
-@main.route('/subpost',methods=['POST','GET'])
-@login_required
-def sub_post():
-    if request.method== 'POST':
-        title= request.form["title"]
-        tag = request.form["tag"]
-        content = request.form["content"]
-        #form.title.data=""
-        #form.content.data=''
-        #form.tag.data=""
-        post = Post(title=title,tag=tag,content=content)
-        db.session.add(post)
-        db.session.commit()
-        return  redirect(url_for(".post",id=post.id))
-
-    return render_template('sub_post_v2.html')
-
-@main.route('/edit/<int:id>',methods=['POST','GET'])
-@login_required
-def edit(id):
-    post = Post.query.get_or_404(id)
-    form = PostForm()
-    if form.validate_on_submit():
-        post.title= form.title.data
-        post.content= form.content.data
-        db.session.add(post)
-        db.session.commit()
-        flash("文章已经更新")
-        return  redirect(url_for('.post',id=post.id))
-    form.title.data = post.title
-    form.content.data = post.content
-    return render_template('edit_post.html',form=form,p_id= post.id)
+# @main.route('/p/<int:id>')
+# def post(id):
+#     post = Post.query.get_or_404(id)
+#     content = post.content
+#     tag = post.tag.split(",")
+#     content = md.convert(content)
+#     return render_template('p.html',post=post,content = content,tag=tag)
 
 
-@main.route('/login',methods=['POST','GET'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
-            return redirect(url_for('main.index'))
-        flash('账号或密码错误')
-    return render_template('login.html',form=form)
+# @main.route('/subpost',methods=['POST','GET'])
+# @login_required
+# def sub_post():
+#     if request.method== 'POST':
+#         title= request.form["title"]
+#         tag = request.form["tag"]
+#         content = request.form["content"]
+#         #form.title.data=""
+#         #form.content.data=''
+#         #form.tag.data=""
+#         post = Post(title=title,tag=tag,content=content)
+#         db.session.add(post)
+#         db.session.commit()
+#         return  redirect(url_for(".post",id=post.id))
+#
+#     return render_template('sub_post_v2.html')
+#
+# @main.route('/edit/<int:id>',methods=['POST','GET'])
+# @login_required
+# def edit(id):
+#     post = Post.query.get_or_404(id)
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         post.title= form.title.data
+#         post.content= form.content.data
+#         db.session.add(post)
+#         db.session.commit()
+#         flash("文章已经更新")
+#         return  redirect(url_for('.post',id=post.id))
+#     form.title.data = post.title
+#     form.content.data = post.content
+#     return render_template('edit_post.html',form=form,p_id= post.id)
 
-@main.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash("you have been logged out.")
-    return redirect(url_for('main.index'))
+#
+# @main.route('/login',methods=['POST','GET'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         if user is not None and user.verify_password(form.password.data):
+#             login_user(user,form.remember_me.data)
+#             return redirect(url_for('main.index'))
+#         flash('账号或密码错误')
+#     return render_template('login.html',form=form)
+#
+# @main.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     flash("you have been logged out.")
+#     return redirect(url_for('main.index'))
 
 
-@main.route('/wiki/<word>')
-def wiki(word):
-    from mwclient import  Site
-    import  mwclient
-    ua = 'MyCoolTool. Run by User:mayne. Using mwclient/' + mwclient.__ver__
-    site = mwclient.Site(('http', 'gw2.huiji.wiki'),clients_useragent=ua,path='/')
-    site.login('mayne','since2013')
-    page = site.pages[word]
-    page_text = page.text()[0:50]
-    return  page_text
-
-@main.route('/search')
-def search():
-    pass
-
-@main.route('/test')
-def webtest():
-    return render_template('logint.html')
+# @main.route('/wiki/<word>')
+# def wiki(word):
+#     from mwclient import  Site
+#     import  mwclient
+#     ua = 'MyCoolTool. Run by User:mayne. Using mwclient/' + mwclient.__ver__
+#     site = mwclient.Site(('http', 'gw2.huiji.wiki'),clients_useragent=ua,path='/')
+#     site.login('mayne','since2013')
+#     page = site.pages[word]
+#     page_text = page.text()[0:50]
+#     return  page_text
+#
+# @main.route('/search')
+# def search():
+#     pass
+#
+# @main.route('/test')
+# def webtest():
+#     return render_template('logint.html')
