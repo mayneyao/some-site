0a860014e846e8a28933e689a187c1b0b8625ef3
mayne <dreama9ain@gmail.com>
2018-08-28 16:06:51 +0800

    [ADD] webhook更新blog

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,14 @@
-from django.http import JsonResponse
-
-from django.views.decorators.csrf import csrf_exempt
 import json
+import subprocess
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 def webhook(request):
     data = json.loads(request.body)
+    repository = data['repository']['name']
+    if repository == 'blog':
+        subprocess.check_output('git', '-C', '/root/blog', 'pull')
     return JsonResponse(data)
